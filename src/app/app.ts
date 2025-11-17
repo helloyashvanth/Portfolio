90eec6953fe730f72e1337df78c93646d8f1b5da
helloyashvanth <hello.yashvanth@gmail.com>
2025-11-17 08:20:31 +0530

    Projects Component completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { NavComponent } from "./components/nav-component/nav-component";
 import { HeroComponent } from "./components/hero-component/hero-component";
 import { SkillsComponent } from "./components/skills-component/skills-component";
+import { ProjectsComponent } from "./components/projects-component/projects-component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavComponent, HeroComponent, SkillsComponent],
+  imports: [NavComponent, HeroComponent, SkillsComponent, ProjectsComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
